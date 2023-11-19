0c47933e2d52ab769563ab113f3e0ed2fa2734fb
M. Julianda al mubarroq <147566714+Mr07Nandut@users.noreply.github.com>
2023-11-19 11:06:12 +0700

    Add files via upload

---
+++ Latihan5a.php
@@ -0,0 +1,14 @@
+
+<?php
+// Periksa apakah parameter 'angka' ada pada URL
+$angka = isset($_GET['angka']) ? $_GET['angka'] : 10;
+
+// Membuat rangkaian angka terurut
+for ($i = 0; $i < $angka; $i++) {
+    for ($j = 1; $j <= $angka - $i; $j++) {
+        echo $j . ' ';
+    }
+    echo '<br>';
+}
+?>
+
